54c65c741bf6742861033a177f9c530727ab2d89
tonio155 <tonio155@gmail.com>
2017-01-01 10:54:53 +0000

    2.0: new project added

---
+++ Project2-Clock JS CSS/js/main.js
@@ -0,0 +1,18 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hoursHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const mins = now.getMinutes();
+  const hours = now.getHours();
+  const secondsDegrees = ((seconds/60)*360) + 90;
+  const minsDegrees = ((mins/60)*360) + ((seconds/60)*6) + 90;
+  const hoursDegrees = ((hours/12) * 360) + ((mins/60)*30) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
